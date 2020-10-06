fe4663096d72eccfdd2fae939f92f4672c16325b
gage <fgawf@awge.com>
2020-10-06 09:39:57 +0900

    範囲外に出ないようにした

--- Classes/Check/CheckObjHit.cpp
+++ Classes/Check/CheckObjHit.cpp
@@ -4,20 +4,33 @@ USING_NS_CC;
 
 bool CheckObjHit::operator()(cocos2d::Sprite & sprite, ActModule & module)
 {
+	auto director = Director::getInstance();
+	if ((TMXLayer*)director->getRunningScene()->getChildByName("BG_BACK")->getChildByName("MapData") == nullptr)
+	{
+		return false;
+	}
 	const int chipSize = 48;
 	auto plPos = sprite.getPosition();
-	auto director = Director::getInstance();
 	auto CollisionData = (TMXLayer*)director->getRunningScene()->getChildByName("BG_BACK")->getChildByName("MapData")->getChildByName("col");
 	auto ColSize = CollisionData->getLayerSize();
-
 	auto plCheckPoint1 = plPos + module.checkPoint1;
 	auto plCheckPoint2 = plPos + module.checkPoint2;
+
+	
 
 	auto plCheckPoint1Chip = Vec2{ plCheckPoint1 } / chipSize;
 	auto plCheckPoint2Chip = Vec2{ plCheckPoint2 } / chipSize;
 
 	auto plCheckPoint1Pos = Vec2(plCheckPoint1Chip.x, ColSize.height - plCheckPoint1Chip.y);
 	auto plCheckPoint2Pos = Vec2(plCheckPoint2Chip.x, ColSize.height - plCheckPoint2Chip.y);
+
+	// �͈͊Ocheck
+	if (plCheckPoint1Pos.x > ColSize.width || plCheckPoint1Pos.x < 0 ||
+		plCheckPoint1Pos.y > ColSize.height || plCheckPoint1Pos.y < 0)
+	{
+		return false;
+	}
+
 
 	auto plCheckPoint1Gid = CollisionData->getTileGIDAt(plCheckPoint1Pos);
 	auto plCheckPoint2Gid = CollisionData->getTileGIDAt(plCheckPoint2Pos);
