ba829d527744355beaa3185cd2b16d8abdbc19c3
kimura <fgawf@awge.com>
2020-10-08 18:57:58 +0900

    ぶつかったためマージ

--- Classes/GameMap.cpp
+++ Classes/GameMap.cpp
@@ -9,18 +9,37 @@ void GameMap::CreateMap(cocos2d::Layer& layer, std::string omotePath, std::strin
 	{
 		createMapFromPath(uraPath, layer);
 	} 
-	//maps_[1].setPosition(maps_[1].)
+	//maps_[0]->setPositionZ(-50);
+	maps_[1]->setVisible(false);
 	mapLayer_ = &layer;	
+	
 }
 
 void GameMap::ReplaceMap()
 {
 }
 
-void GameMap::createMapFromPath(std::string& omotePath, cocos2d::Layer& layer)
+cocos2d::TMXTiledMap* GameMap::GetMap()
 {
-	TMXTiledMap* map = TMXTiledMap::create(omotePath);
+	return maps_[static_cast<int>(mapType_)];
+}
+
+void GameMap::SetMapInfo(MapType mapType)
+{
+	maps_[static_cast<int>(mapType)]->setVisible(true);
+	maps_[(static_cast<int>(mapType) ^! 0)]->setVisible(false);
+	mapType_ = mapType;
+}
+
+
+void GameMap::createMapFromPath(std::string& path, cocos2d::Layer& layer)
+{
+	
+	TMXTiledMap* map = TMXTiledMap::create(path);
+	map->setName("MapData");
+	auto colLayer = map->getLayer("Collision");
+	colLayer->setName("col");
 	layer.addChild(map);
 	//map->setPosition(map->getPosition());
-	//maps_.emplace_back(map);
+	maps_.emplace_back(map);
 }
