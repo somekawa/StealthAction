2f1aa963cacad52b91e316502f45e315e93ad586
1816013 <iahliugaeligfaliugefliw.com>
2021-02-17 16:54:49 +0900

    マージエラー解除

--- Classes/Shader/ResShadowShader.h
+++ Classes/Shader/ResShadowShader.h
@@ -13,19 +13,3 @@ private:
 
 	cocos2d::backend::Program* program;
 };
-
-#pragma once
-#include <cocos2d.h>
-class ResShadowShader
-{
-public:
-	ResShadowShader();
-	~ResShadowShader();
-	// ノードにシェーダを設定
-	void SetShader(cocos2d::Node& node);
-
-private:
-
-	cocos2d::backend::Program* program;
-};
-
